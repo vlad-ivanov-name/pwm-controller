567ae22a71c13d36ca2cb3a638820a7256ac89ee
Vlad Ivanov <vlad-mbx@ya.ru>
2015-08-21 11:03:05 +0300

    Fix instance addressing

--- pwm_controller_top.v
+++ pwm_controller_top.v
@@ -14,6 +14,10 @@ module pwm_controller_top (
 	wire [7:0] b_data_r;
 	wire [7:0] b_data_w;
 	wire       b_write;
+	
+	wire [4:0] instance_addr;
+	wire [`PWM_INSTANCES - 1:0] instance_select;
+	wire [7:0] instance_rdata [`PWM_INSTANCES - 1:0];
 
 	genvar instance_index;
 
@@ -32,16 +36,20 @@ module pwm_controller_top (
 
 	generate
 		for (instance_index = 0; instance_index < `PWM_INSTANCES; instance_index = instance_index + 1) begin: i_pwm_gen
+			assign instance_select[instance_index] = instance_addr == instance_index;
 			pwm i_pwm (
 				.b_addr_i (b_addr  ),
 				.b_data_i (b_data_w),
-				.b_data_o (b_data_r),
-				.b_write_i(b_write ),
+				.b_data_o (instance_rdata[instance_index]),
+				.b_write_i(b_write & instance_select[instance_index]),
 				.clk_i    (clk_i   ),
 				.nrst_i   (nrst_i  ),
 				.pwm_o    (pwm_o   )
 			);
 		end
 	endgenerate
+
+	assign instance_addr[4:0] =  b_addr[6:3];
+	assign b_data_r = instance_rdata[instance_addr];
 
 endmodule
